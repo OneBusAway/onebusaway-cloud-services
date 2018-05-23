3cf23bb01ef49b5716e06736d909fce88c18e28b
sheldonabrown <sheldonb@gmail.com>
2018-05-23 09:17:20 -0400

    Initial layout of cloud services maven structure.

---
+++ onebusaway-cloud-noop/src/main/java/org/onebusaway/cloud/noop/ExternalServicesNoopImpl.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C) 2018 Cambridge Systematics, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onebusaway.cloud.noop;
+
+import org.onebusaway.cloud.api.ExternalResult;
+import org.onebusaway.cloud.api.ExternalServices;
+
+public class ExternalServicesNoopImpl implements ExternalServices {
+
+    @Override
+    public ExternalResult pubishMessage(String topic, String messageConents) {
+        return new AlwaysTrueExternalResult();
+    }
+
+    @Override
+    public ExternalResult publishMetric(String metricName, String dimension, Number value) {
+        return new AlwaysTrueExternalResult();
+    }
+}
